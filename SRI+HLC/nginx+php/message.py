c2637aa7a61ba0a7ab4b11d234b2b544463c848b
Belén Nazareth Durán <belennazareth29@gmail.com>
2022-11-18 02:51:10 +0100

    addex

---
+++ SRI+HLC/nginx+php/message.py
@@ -0,0 +1,8 @@
+# Explosión masiva de mensajitos por whatsapp web :)
+
+import pyautogui as pt
+import time
+
+limit = input ("·.* Cantidad de mensajitos: ")
+message = input ("·.* Mensajito: ")
+i = 0
